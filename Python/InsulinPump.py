1857d1695728154e0997d40b755c2023018a661b
Appsie <appsie095@hotmail.com>
2017-10-24 20:08:13 +1000

    Organising Directory

---
+++ Python/InsulinPump.py
@@ -0,0 +1,31 @@
+# CP3407 Insulin Pump Python BackEnd
+
+# Python Imports
+
+# Required Functions
+
+# Memory - Database
+
+# Pump Control - inject insulin
+
+# Clock Input
+
+# Alarm
+
+# External Device Connection
+
+# Power Management
+
+# Display??
+
+# Physical Key Input - Key Pad
+
+# Sensors - Temp, Pressure, Blood Glucose level
+
+
+def main():
+    # Main Funcation
+    print("Hello")
+
+if __name__ == '__main__':
+    main()
