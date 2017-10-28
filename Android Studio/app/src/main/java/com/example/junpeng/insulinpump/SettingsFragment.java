3c35ec25720ddd5fa52471424d900b7c3e93f1d4
Robert Warnes <robert.warnes@my.jcu.edu.au>
2017-10-29 04:31:54 +1000

    Drip and Settings

--- Android Studio/app/src/main/java/com/example/junpeng/insulinpump/SettingsFragment.java
+++ Android Studio/app/src/main/java/com/example/junpeng/insulinpump/SettingsFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SettingsFragment extends Fragment {
     public static SettingsFragment newInstance() {
@@ -15,11 +16,14 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.settings_fragment, container, false);
     }
 }
