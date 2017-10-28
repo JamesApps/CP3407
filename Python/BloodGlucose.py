81a8e5588e926efe6485c77c837a91cbe7228a57
Bncbck <rameshd1806@gmail.com>
2017-10-29 05:31:24 +1000

    Insulin Reservoir/Needle Assembly

--- Python/BloodGlucose.py
+++ Python/BloodGlucose.py
@@ -11,19 +11,24 @@ class BloodGlucose:
         self.bg = 20.0
         self.safeLevels = [1, 35]
         self.alert = False
+        self.bgFluctuation = 2.0
 
+# runs the update on the blood glucose using the
     def getBG(self):
-        self.bg = self.bg + self.randomGenerator(-2.0, 2.0)
+        self.bg = self.bg + self.randomGenerator(-self.bgFluctuation, self.bgFluctuation )
         if self.bg < 1 or self.bg > 35:
             self.alert = True
         else:
             self.alert = False
-        return self.bg, self.alert
+        return self.bg
 
     # manually sets the safe levels for blood glucose #
     def setSafeLevels(self, safemin, safemax):
         self.safeLevels[0] = safemin
         self.safeLevels[1] = safemax
+
+    def setBGFluctuations(self, value):
+        self.bgFluctuation = value
 
     # adds the stated value to the blood glucose levels, simulating blood glucose adjustment #
     def addBG(self, value):
