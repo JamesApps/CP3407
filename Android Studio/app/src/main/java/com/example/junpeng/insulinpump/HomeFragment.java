ac9674cfb8ba518042689a784fdfeaa24c31195a
Robert Warnes <robert.warnes@my.jcu.edu.au>
2017-10-29 21:21:59 +1000

    Ids Added
    Added viewfindbyId elements and move text from Xml to Java files.

--- Android Studio/app/src/main/java/com/example/junpeng/insulinpump/HomeFragment.java
+++ Android Studio/app/src/main/java/com/example/junpeng/insulinpump/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.example.junpeng.insulinpump;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
     public static HomeFragment newInstance() {
@@ -22,4 +25,20 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.home_fragment, container, false);
     }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        TextView textView = (TextView) getView().findViewById(R.id.textView2);
+        textView.setText("Home");
+        TextView textView1 = (TextView) getView().findViewById(R.id.textView6);
+        textView1.setText("BG/Level");
+        TextView textView2 = (TextView) getView().findViewById(R.id.textView5);
+        textView2.setText("Pump:");
+        TextView textView3 = (TextView) getView().findViewById(R.id.BGlevel);
+        textView3.setText("\\t\\t\\tmmol/l");
+
+        Button setButton = (Button) getView().findViewById(R.id.textManualInjection);
+        setButton.setText("Manaual Injection");
+
+    }
+
 }
