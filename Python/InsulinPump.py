a5147629a964d7f4317f1fad987b86f73d71eb49
Appsie <appsie095@hotmail.com>
2017-10-29 15:25:59 +1000

    Changing database layout

--- Python/InsulinPump.py
+++ Python/InsulinPump.py
@@ -5,13 +5,18 @@ import sqlite3
 from sqlite3 import Error
 import Clock
 
-# Pump configuration parameters
-capacity = 100
-safemin = 6
-safemax = 14
+# Initial/default Pump configuration parameters to store in db
+first_name = 'Joe'
+last_name = 'Do'
+capacity_insulin = 100
+drip_rate = 1
+safe_min = 6
+safe_max = 14
 max_daily_dose = 25
 max_single_dose = 4
 minimum_dose = 1
+cumulative_dose = 0
+
 
 STATE = ""
 
@@ -27,9 +32,10 @@ def create_db():
     table_1_column_field_type = ['INTEGER', 'STRING', 'STRING']  # column data types
 
     table_name2 = 'Insulin'  # name of the table to be created
-    table_2_column = ['insulin_id', 'drip_rate', 'max_insulin_daily', 'remaining_insulin', 'min_insulin_rate',
-                      'max_insulin_rate']  # name of columns
-    table_2_column_field_type = ['INTEGER', 'FLOAT', 'FLOAT', 'FLOAT', 'FLOAT', 'FLOAT']  # column data types
+    table_2_column = ['insulin_id', 'drip_rate', 'max_daily_dose', 'capacity_insulin', 'safe_min', 'safe_max',
+                      'minimum_dose', 'max_single_dose']  # name of columns
+    table_2_column_field_type = ['INTEGER', 'INTEGER', 'INTEGER', 'INTEGER', 'INTEGER', 'INTEGER',
+                                 'INTEGER', 'INTEGER']  # column data types
 
     table_name3 = 'Battery'  # name of the table to be created
     table_3_column = ['battery_id', 'battery_power_level']  # name of columns
@@ -37,7 +43,7 @@ def create_db():
 
     table_name4 = 'Clock'  # name of the table to be created
     table_4_column = ['clock_id', 'clock_time']  # name of columns
-    table_4_column_field_type = ['INTEGER', 'FLOAT']  # column data types
+    table_4_column_field_type = ['INTEGER', 'TIME']  # column data types
 
     table_name5 = 'Blood_Glucose'  # name of the table to be created
     table_5_column = ['blood_glucose_id', 'blood_glucose_level']  # name of columns
@@ -45,8 +51,8 @@ def create_db():
 
     table_name6 = 'Information_Log'  # name of the table to be created
     table_6_column = ['information_log_id', 'clock_time', 'battery_power_level', 'remaining_insulin',
-                      'blood_glucose_level']  # name of columns
-    table_6_column_field_type = ['INTEGER', 'FLOAT', 'FLOAT', 'FLOAT', 'INTEGER']  # column data types
+                      'cumulative_dose', 'blood_glucose_level']  # name of columns
+    table_6_column_field_type = ['INTEGER', 'TIME', 'INTEGER', 'INTEGER', 'INTEGER', 'INTEGER']  # column data types
 
     try:
         # Connecting to the database file
@@ -91,6 +97,8 @@ def create_db():
                   .format(tn=table_name6, nf=table_6_column[0], ft=table_6_column_field_type[0]))
         for column, field in zip(table_6_column[1:], table_6_column_field_type[1:]):
             c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}".format(tn=table_name6, cn=str(column), ct=field))
+
+            # Initialise single value table entries
 
 
 def state_run(insulin_available, cumulative_dose, r0, r1, r2):
@@ -145,47 +153,47 @@ def get_compdose(r0, r1, r2):
     # before that. These are used to compute the rate of change of blood sugar readings
 
     # SUGAR_LOW
-    if r2 < safemin:
+    if r2 < safe_min:
         compdose = 0
         # alarm low sugar
 
     # SUGAR_OK
-    elif safemin <= r2 <= safemax:
+    elif safe_min <= r2 <= safe_max:
         # sugar level stable or falling
         if r2 <= r1:
             compdose = 0
 
         # sugar level increasing but rate of increase falling
-        elif r2 > r1 and (r2-r1) < (r1-r0):
+        elif r2 > r1 and (r2 - r1) < (r1 - r0):
             compdose = 0
 
         # sugar level increasing and rate of increase increasing compute dose a minimum dose must be delivered
         # if rounded to zero
-        elif r2 > r1 and (r2-r1) >= (r1-r0) and (round((r2-r1)/4) == 0):
+        elif r2 > r1 and (r2 - r1) >= (r1 - r0) and (round((r2 - r1) / 4) == 0):
             compdose = minimum_dose
 
-        elif r2 > r1 and (r2-r1) >= (r1-r0) and (round((r2-r1)/4) > 0):
-            compdose = round((r2-r1)/4)
+        elif r2 > r1 and (r2 - r1) >= (r1 - r0) and (round((r2 - r1) / 4) > 0):
+            compdose = round((r2 - r1) / 4)
 
     # SUGAR_HIGH
-    elif r2 > safemax:
+    elif r2 > safe_max:
         # sugar level increasing. Round down if below 1 unit.
-        if r2 > r1 and (round((r2-r1)/4) == 0):
+        if r2 > r1 and (round((r2 - r1) / 4) == 0):
             compdose = minimum_dose
 
-        elif r2 > r1 and (round((r2-r1)/4) > 0):
-            compdose = round((r2-r1)/4)
+        elif r2 > r1 and (round((r2 - r1) / 4) > 0):
+            compdose = round((r2 - r1) / 4)
 
         # sugar level stable
         elif r2 == r1:
             compdose = minimum_dose
 
         # sugar level falling and rate of decrease increasing
-        elif r2 < r1 and (r2-r1) <= (r1-r0):
+        elif r2 < r1 and (r2 - r1) <= (r1 - r0):
             compdose = 0
 
         # sugar level falling and rate of decrease decreasing
-        elif r2 < r1 and (r2-r1) > (r1-r0):
+        elif r2 < r1 and (r2 - r1) > (r1 - r0):
             compdose = minimum_dose
     return compdose
 
@@ -207,8 +215,8 @@ def state_startup():
     # This means that the total cumulative dose delivered can always be tracked by the system
     # and is not affected by the user switching the machine on and off.
     dose = 0
-    r0 = safemin
-    r1 = safemax
+    r0 = safe_min
+    r1 = safe_max
     # TEST
     return dose, r0, r1
 
@@ -238,7 +246,7 @@ def state_test(needle, reservoir, insulin_available, battery, pump, sensor, deli
         alarm("reservoir_removed")
     elif insulin_available < max_single_dose:
         alarm("low_insulin")
-    elif battery < 0.5:
+    elif battery < 5:
         alarm("battery_low")
     elif not pump:
         alarm("pump_failure")
@@ -250,10 +258,9 @@ def state_test(needle, reservoir, insulin_available, battery, pump, sensor, deli
         print('Passed Test')
 
 
-
 def alarm(function):
     # Alarm Functions
-    # Battery low - The voltage of the battery has fallen to less than 0.5V
+    # Battery low - The voltage of the battery has fallen to less than 5%
     if function == "battery_low":
         print("battery_low")
 
@@ -292,16 +299,16 @@ def main():
     if clock.hours == 0 and clock.minutes == 0 and clock.seconds == 0:
         cumulative_dose = 0
 
-
     # SQLite Database
     if os.path.isfile(sqlite_file):
         pass
     else:
         create_db()
 
+    # Set Up
+
     dose, r0, r1 = state_startup()
     print(dose, r0, r1)
-
 
     # Logging Loop
     while True:
