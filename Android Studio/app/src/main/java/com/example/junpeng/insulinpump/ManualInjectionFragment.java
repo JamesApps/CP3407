687c3665c94dbd17da4a02f26710b6bdf168ee92
Robert Warnes <robert.warnes@my.jcu.edu.au>
2017-10-30 17:35:47 +1000

    Id and Text adjustments

--- Android Studio/app/src/main/java/com/example/junpeng/insulinpump/ManualInjectionFragment.java
+++ Android Studio/app/src/main/java/com/example/junpeng/insulinpump/ManualInjectionFragment.java
@@ -1,4 +1,5 @@
 package com.example.junpeng.insulinpump;
 
+
 public class ManualInjectionFragment {
 }
--- Android Studio/app/src/main/java/com/example/junpeng/insulinpump/Signup_Fragment.java
+++ Android Studio/app/src/main/java/com/example/junpeng/insulinpump/Signup_Fragment.java
@@ -1,24 +1,25 @@
-package layout;
+package com.example.junpeng.insulinpump;
 
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.junpeng.insulinpump.R;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Sign_in_Fragment.OnFragmentInteractionListener} interface
+ * {@link Signup_Fragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Sign_in_Fragment#newInstance} factory method to
+ * Use the {@link Signup_Fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Sign_in_Fragment extends Fragment {
+public class Signup_Fragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +31,7 @@ public class Sign_in_Fragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public Sign_in_Fragment() {
+    public Signup_Fragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +41,11 @@ public class Sign_in_Fragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Sign_in_Fragment.
+     * @return A new instance of fragment Signup_Fragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Sign_in_Fragment newInstance(String param1, String param2) {
-        Sign_in_Fragment fragment = new Sign_in_Fragment();
+    public static Signup_Fragment newInstance(String param1, String param2) {
+        Signup_Fragment fragment = new Signup_Fragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,7 +66,7 @@ public class Sign_in_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in_, container, false);
+        return inflater.inflate(R.layout.fragment_signup_, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -105,5 +106,19 @@ public class Sign_in_Fragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        TextView textView = (TextView) getView().findViewById(R.id.signuptext);
+        textView.setText("Sign Up");
+        TextView textView1 = (TextView) getView().findViewById(R.id.textusername);
+        textView1.setText("Username: ");
+        TextView textView2 = (TextView) getView().findViewById(R.id.textemail);
+        textView2.setText("Email: ");
+        TextView textView3 = (TextView) getView().findViewById(R.id.textpassword);
+        textView3.setText("Password: ");
+
+        Button setButton = (Button) getView().findViewById(R.id.bn);
+        setButton.setText("Sign Up");
     }
 }
