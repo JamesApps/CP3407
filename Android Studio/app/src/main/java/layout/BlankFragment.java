6ee38c1223e323a99f6b47c8aa1dbac05dea5927
JunpengGao <303180451@qq.com>
2017-10-30 13:56:52 +1100

    ManualInjectionFragment

--- Android Studio/app/src/main/java/layout/BlankFragment.java
+++ Android Studio/app/src/main/java/layout/BlankFragment.java
@@ -65,7 +65,7 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        return inflater.inflate(R.layout.manual_injection_fragment_, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
